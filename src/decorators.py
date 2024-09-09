f1b49f0d40de75b24f28725fd33fe156fcb284dd
Nataly Veldina <221fuchs@gmail.com>
2024-09-09 22:58:32 +0500

    update decorators.py test_decorators.py README.md

--- src/decorators.py
+++ src/decorators.py
@@ -1,12 +1,25 @@
-from functools import wraps
+def log(filename=""):
+    """Декоратор регистрирует данные выполнения функций"""
 
-def decorator(func):
-    @wraps(func)
-    def wrapper(*args):
-       pass
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            try:
+                result = func(*args, **kwargs)
+                if filename:
+                    with open(filename, "w") as file:
+                        file.write(f"Функция {func.__name__} работает")
+                else:
+                    print(f"{func.__name__} работает")
+                return result
+            except Exception as ex:
+                if filename:
+                    with open(filename, "w") as file:
+                        file.write(
+                            f"Ошибка в функции {func.__name__}: {ex.__class__.__name__}. Параметры: {args}, {kwargs}"
+                        )
+                else:
+                    print(f"Ошибка в функции {func.__name__}: {ex.__class__.__name__}. Параметры: {args}, {kwargs}")
+
         return wrapper
-    return decorator
 
-@log(filename='mylog.txt')
-def log(filename):
-    if filename:
+    return decorator
